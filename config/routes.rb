ff9e7a6f2836940e411384fc3cd764f8fa053362
Gary <gkwheeler40@gmail.com>
2020-12-17 17:49:02 -0700

    rails generate comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :users
       resources :loads
       resources :directions
+      resources :comments
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
