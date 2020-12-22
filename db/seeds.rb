f6623d18ea32c68e8792e209a40322e88c6b2712
Gary <gkwheeler40@gmail.com>
2020-12-21 20:33:43 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Load.destroy_all
+Direction.destroy_all
+Comment.destroy_all
+
+steve = User.create(username: "Sstump", truck_number: 10, password: "password")
+
+steve_load = Load.create(customer_name: "Munch Supply", city: "New Lenox", state: "Il", user_id: 1)
+
+Direction.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.", load_id: 1)
+
+Comment.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 1, direction_id: 1)
