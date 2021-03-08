3d5d69c57335095e63a06015c120e9cc5d94f2f0
Gary <gkwheeler40@gmail.com>
2021-03-08 02:10:12 -0700

    add more seed data for testing in frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,55 @@ Load.destroy_all
 Direction.destroy_all
 Comment.destroy_all
 
-steve = User.create(username: "Sstump", truck_number: 10, password: "password")
+User.create(username: "Sstump", truck_number: 10, password: "password")
+User.create(username: "Tlee", truck_number: 8, password: "password")
 
-steve_load = Load.create(customer_name: "Munch Supply", city: "New Lenox", state: "Il", user_id: 1)
+Load.create(customer_name: "Munch Supply", city: "New Lenox", state: "Il", user_id: 1)
+Load.create(customer_name: "Robertson Supply", city: "Alliance", state: "OH", user_id: 1)
+
+Load.create(customer_name: "Ferguson Supply", city: "Lakewood", state: "NJ", user_id: 2)
+Load.create(customer_name: "Best Choice Supply", city: "Lexington", state: "KY", user_id: 2)
 
 Direction.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
     nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.", load_id: 1)
 
+Direction.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.", load_id: 2)
+
+Direction.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.", load_id: 3)
+
+Direction.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.", load_id: 4)
+
 Comment.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
     incididunt ut labore et dolore magna aliqua.", user_id: 1, direction_id: 1)
+
+Comment.create(content: "Comment 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 1, direction_id: 1)
+
+Comment.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 1, direction_id: 2)
+
+Comment.create(content: "Comment 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 1, direction_id: 2)
+
+Comment.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 2, direction_id: 3)
+
+Comment.create(content: "Comment 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 2, direction_id: 3)
+
+Comment.create(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 2, direction_id: 4)
+
+Comment.create(content: "Comment 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.", user_id: 2, direction_id: 4)
